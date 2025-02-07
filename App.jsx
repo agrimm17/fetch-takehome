c415529693acb04907ca9a030d854206dc9956bc
agrimm17 <alexandergrimm17@gmail.com>
2025-02-07 12:07:47 -0800

    hosting page

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello Dogs</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ index.js
@@ -0,0 +1,26 @@
+// Login that connects to Fetch API
+    // Name + email
+    // pass a config option to send credentials (cookies) with future requests
+
+// Search Page
+    // Filter by breed
+    // Paginated
+    // sorted alphabetically by breed
+        // Can be reversed
+    // display all dog object (except ID)
+    // Users can select favs
+    // User can "generate match" from favs using /dogs/match endpoint
+        // display single match
+    
+//investigate component library?
+// App must be hosted on internet
+// source code should be stored in git repo
+// include documentation to run app locally
+
+import App from './App.jsx'
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.querySelector('#root'))
+root.render(<App />);
+
+
